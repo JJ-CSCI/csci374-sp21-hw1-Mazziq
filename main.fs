3807f6b3355e764249a5740b41e4f8d054399cd1
Mazziq <snoziznasty@gmail.com>
2021-03-15 03:59:38 +0000

    1

--- main.fs
+++ main.fs
@@ -3,7 +3,10 @@ module Assignment
 // Problem 1
 let rec tribn n =
     // write your code here
-    0
+    if n <= 0 then 0
+    else if n = 1 then 1
+    else if n = 2 then 1
+    else tribn (n-1) + tribn (n-2) + tribn (n-3)
 
 // Problem 2
 let tribn2 n =
